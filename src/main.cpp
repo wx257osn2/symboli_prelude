b28933c7985c7271aad9a195364e107e37eabd0c
I <wx257osn2@yahoo.co.jp>
2021-09-11 20:10:34 +0900

    add console

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,26 @@
 #include<optional>
 #include<filesystem>
 #include<string_view>
+#include<fstream>
 #include<symboli/hook.hpp>
+#include<nlohmann/json.hpp>
+#include<will/console.hpp>
 
 using namespace std::literals::string_view_literals;
 
 static std::optional<symboli::hook> hook_dll;
+
+struct config_t{
+	bool enable_console;
+}static config = {
+	.enable_console = false
+};
+static inline void to_json(nlohmann::json& j, const config_t& conf){
+	j = nlohmann::json{{"enable_console", conf.enable_console}};
+}
+static inline void from_json(const nlohmann::json& j, config_t& conf){
+	j.at("enable_console").get_to(conf.enable_console);
+}
 
 struct load_library_w : symboli::hook::func<decltype(::LoadLibraryW), load_library_w>{
 	static HMODULE WINAPI func(LPCWSTR lib_filename){
@@ -21,11 +36,36 @@ struct load_library_w : symboli::hook::func<decltype(::LoadLibraryW), load_libra
 	}
 };
 
+will::expected<void> create_console(const TCHAR* console_title)try{
+	if(!will::console::attach(ATTACH_PARENT_PROCESS))
+		will::console::alloc().value();
+	will::console::bind_stdstreams().value();
+	will::console::set_output_cp(CP_UTF8).value();
+	std::locale::global(std::locale(""));
+	will::console::set_title(console_title).value();
+	return {};
+}catch(...){
+	return will::make_unexpected_from_current_exception();
+}
+
 static inline BOOL process_attach(HINSTANCE hinst){
 	const std::filesystem::path plugin_path{will::get_module_file_name(hinst).value()};
 	hook_dll = symboli::hook::create(plugin_path.parent_path()/"symboli_hook.dll").value();
 	hook_dll->enqueue_hook<load_library_w>(&::LoadLibraryW).value();
 	hook_dll->consume_queue();
+
+	std::ifstream config_file{plugin_path.parent_path()/"symboli_prelude.config"};
+	if(config_file.is_open())try{
+		nlohmann::json j;
+		config_file >> j;
+		config = j.get<config_t>();
+	}catch(std::exception& e){
+		::MessageBoxA(nullptr, e.what(), "Symboli Prelude exception", MB_OK|MB_ICONWARNING|MB_SETFOREGROUND);
+	}
+
+	if(config.enable_console){
+		create_console(_T("Symboli console")).value();
+	}
 	return TRUE;
 }
 
